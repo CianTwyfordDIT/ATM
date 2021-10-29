755cff88f24333ca05eabeb32b78a992b529cf6f
Cian Twyford <cian.twyford@outlook.ie>
2021-10-29 18:13:42 +0100

    Create account and add to array

--- ATM/src/Account.java
+++ ATM/src/Account.java
@@ -1,12 +1,55 @@
 
 public class Account 
 {
-	int accountID = 0;
-	String custName;
+	static int numAccounts;
+	private int accountID;
+	private String custName;
+	private int PIN;
+	private double balance = 0;
 	
-	Account(String custName)
+	Account(String custName, int PIN, double balance)
 	{
-		this.accountID = accountID++;
+		this.setAccountID();
+		this.setCustName(custName);
+		this.setPIN(PIN);
+		this.setBalance(balance);
+		numAccounts++;
+	}
+	
+	public void setAccountID()
+	{
+		this.accountID = numAccounts;
+	}
+	
+	public void setCustName(String custName)
+	{
 		this.custName = custName;
 	}
+	
+	public void setPIN(int PIN)
+	{
+		this.PIN = PIN;
+	}
+	
+	public void setBalance(double balance)
+	{
+		this.balance = balance;
+	}
+	
+	public int getAccountID()
+	{
+		return this.accountID;
+	}
+	
+	public String getCustName()
+	{
+		return this.custName;
+	}
+	
+	public String toString()
+	{
+		return "Account ID: " + accountID + "\nCustomer Name: "
+				+ custName + "\nBalance: " + balance;
+	}
+	
 }
--- ATM/src/Main.java
+++ ATM/src/Main.java
@@ -5,11 +5,19 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-		displayLogIn();
+		Account[] accounts = new Account[50];
+		
+		Account account1 = new Account("Cian Twyford", 1234, 2000.00);
+		accounts[accounts.length-(accounts.length)] = account1;
+		
+		displayAccounts(accounts);
+		
+		//displayLogIn(accounts);
 	}
 	
-	public static void displayLogIn()
+	public static void displayLogIn(Account[] accounts)
 	{
+		int response;
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("*******************\n");
@@ -17,8 +25,23 @@ public class Main
 		System.out.print("*******************\n\n");
 		System.out.println("1) Log In");
 		System.out.println("2) Create Account");
+		System.out.print("\nPlease Select An Option: ");
+		
+		response = scanner.nextInt();
 		
 		scanner.close();
 	}
-
+	
+	public static void displayAccounts(Account[] accounts)
+	{
+		for(int i = 0; i < accounts.length; i++)
+		{
+			if(accounts[i] == null)
+			{
+				continue;
+			}
+			System.out.println(accounts[i]);
+		}
+		
+	}
 }
