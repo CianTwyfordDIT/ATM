2f96a306104de55838483af0ecb8f23249af7e72
Cian Twyford <cian.twyford@outlook.ie>
2021-12-08 14:20:11 +0000

    Main Options Menu

--- ATM/src/Main.java
+++ ATM/src/Main.java
@@ -103,6 +103,7 @@ public class Main
 		int response;
 		boolean checkResponse = false;
 		Scanner scanner = new Scanner(System.in);
+		accounts = awr.Read();
 		
 		System.out.print("*******************\n");
 		System.out.print("\033[34m");
@@ -156,7 +157,7 @@ public class Main
 						System.out.println("Login Successful");
 						System.out.print("\033[37m");
 						loading();
-						mainOptions();
+						mainOptions(accounts);
 					}
 					else
 					{
@@ -177,6 +178,14 @@ public class Main
 				}
 				catch(InputMismatchException e)
 				{
+					System.out.print("\033[31m");
+					System.out.println("Invalid Input");
+					System.out.print("\033[37m");
+
+					System.out.println("\nPress Enter to Continue:");
+					scanner.nextLine();
+					scanner.nextLine();
+					
 					logIn(accounts);
 				}
 			}
@@ -200,6 +209,14 @@ public class Main
 		}
 		catch(InputMismatchException e)
 		{
+			System.out.print("\033[31m");
+			System.out.println("Invalid Input");
+			System.out.print("\033[37m");
+			
+			System.out.println("\nPress Enter to Continue:");
+			scanner.nextLine();
+			scanner.nextLine();
+
 			logIn(accounts);
 		}
 		finally
@@ -242,90 +259,164 @@ public class Main
 				
 				cls();
 				loading();
-				cls();
-				System.out.print("\033[33m");
-				System.out.println("Confirm Details");
-				System.out.print("\033[37m");
-				System.out.println("\nCustomer Name: " + custName);
-				System.out.println("PIN: " + PIN);
-				System.out.println("Balance: $" + balance);
-				System.out.print("\033[33m");
-				System.out.print("Y/N (Enter 0 to return to main menu): ");
-				System.out.print("\033[37m");
 				
-				String response = scanner.nextLine();
+				String response;
 				
-				if(response.equals("Y") || response.equals("y"))
+				do
 				{
-					confirmation = true;
-					int accountID = accounts.size()+1;
-					accounts.add(new Account(custName, PIN, balance, accountID));
-					accounts = awr.Write(accounts);
 					cls();
-					System.out.print("\033[32m");
-					System.out.println("New Account Created");
-					System.out.print("\033[36m");
-					System.out.println("\nUser Login Details:");
-					System.out.println("\033[33mAccount Number: \033[37m" + accounts.get(accounts.size()-1).getAccountID());
-					System.out.println("\033[33mPIN: \033[37m" + accounts.get(accounts.size()-1).getPIN());
 					
-					System.out.println("\nPress Enter to Continue:");
-					scanner.nextLine();
-					
-					displayOpeningScreen(accounts);
-					
-				}
-				else if(response.equals("N") || response.equals("n"))
-				{
-					confirmation = false;
-					cls();
-					loading();
-				}
-				else if(response.equals("0"))
-				{
-					displayOpeningScreen(accounts);
-				}
-				else
-				{
-					confirmation = false;
-					System.out.print("\033[31m");
-					System.out.println("Invalid Input");
+					System.out.print("\033[33m");
+					System.out.println("Confirm Details");
 					System.out.print("\033[37m");
-					try 
+					System.out.println("\nCustomer Name: " + custName);
+					System.out.println("PIN: " + PIN);
+					System.out.println("Balance: $" + balance);
+					System.out.print("\033[33m");
+					System.out.print("Y/N (Enter 0 to return to main menu): ");
+					System.out.print("\033[37m");
+					
+					response = scanner.nextLine();
+					
+					if(response.equals("Y") || response.equals("y"))
 					{
-						Thread.sleep(3000);
-					} 
-					catch (InterruptedException ee) 
-					{
-						// TODO Auto-generated catch block
-						ee.printStackTrace();
+						confirmation = true;
+						int accountID = accounts.size()+1;
+						accounts.add(new Account(custName, PIN, balance, accountID));
+						accounts = awr.Write(accounts);
+						cls();
+						System.out.print("\033[32m");
+						System.out.println("New Account Created");
+						System.out.print("\033[36m");
+						System.out.println("\nUser Login Details:");
+						System.out.println("\033[33mAccount Number: \033[37m" + accounts.get(accounts.size()-1).getAccountID());
+						System.out.println("\033[33mPIN: \033[37m" + accounts.get(accounts.size()-1).getPIN());
+						
+						System.out.println("\nPress Enter to Continue:");
+						scanner.nextLine();
+						
+						displayOpeningScreen(accounts);
+						
 					}
-				}
+					else if(response.equals("N") || response.equals("n"))
+					{
+						confirmation = false;
+						cls();
+						loading();
+					}
+					else if(response.equals("0"))
+					{
+						displayOpeningScreen(accounts);
+					}
+					else
+					{
+						confirmation = false;
+						System.out.print("\033[31m");
+						System.out.println("Invalid Input");
+						System.out.print("\033[37m");
+	
+						System.out.println("\nPress Enter to Continue:");
+						scanner.nextLine();
+					}
+				} while(!response.equals("Y") && !response.equals("y") && !response.equals("N") && !response.equals("n") && !response.equals("0"));
 			}
 			catch(InputMismatchException e)
 			{
 				System.out.print("\033[31m");
 				System.out.println("Invalid Input");
 				System.out.print("\033[37m");
+				
+				System.out.println("\nPress Enter to Continue:");
+				scanner.nextLine();
+				scanner.nextLine();
+				
+				displayCreateAccount(accounts);
+			}
+		} while(confirmation == false);
+	}
+	
+	public static void mainOptions(ArrayList<Account> accounts)
+	{
+		cls();
+		Scanner scanner = new Scanner(System.in);
+		int response;
+		
+		System.out.print("*******************\n");
+		System.out.print("\033[34m");
+		System.out.print("Bank Of Ireland\n");
+		System.out.print("\033[37m");
+		System.out.print("*******************\n\n");
+		System.out.println("1) Check Balance & History");
+		System.out.println("2) Withdraw Cash");
+		System.out.println("3) Deposit Cash");
+		System.out.println("4) Change PIN");
+		System.out.println("5) Delete Account");
+		System.out.println("6) Exit");
+		System.out.print("\nPlease Select An Option: ");	
+		
+		try
+		{
+			response = scanner.nextInt();
+			
+			switch(response)
+			{
+			case 1:
+				break;
+				
+			case 2:
+				break;
+				
+			case 3:
+				break;
+			
+			case 4:
+				break;
+				
+			case 5:
+				break;
+				
+			case 6:
+				cls();
+				loading();
+				displayOpeningScreen(accounts);
+			
+			default:
+				System.out.print("\033[31mInvalid Selection");
+				System.out.println("\033[37m");
 				try 
 				{
 					Thread.sleep(3000);
 				} 
-				catch (InterruptedException ee) 
+				catch (InterruptedException e) 
 				{
 					// TODO Auto-generated catch block
-					ee.printStackTrace();
+					e.printStackTrace();
 				}
-				displayCreateAccount(accounts);
+				cls();
+				mainOptions(accounts);
 			}
 		}
-		while(confirmation == false);
-	}
-	
-	public static void mainOptions()
-	{
-		cls();
-		System.out.println("Hi");
+		catch(InputMismatchException e)
+		{
+			System.out.print("\033[31m");
+			System.out.print("Invalid Selection");
+			System.out.println("\033[37m");
+			try 
+			{
+				Thread.sleep(3000);
+			} 
+			catch (InterruptedException ee) 
+			{
+				// TODO Auto-generated catch block
+				ee.printStackTrace();
+			}
+			cls();
+			mainOptions(accounts);
+		}
+		finally
+		{
+			scanner.close();
+		}
 	}
 	
 	public static void displayAccounts(ArrayList<Account> accounts)
